b4a9945b1ee41c852e5ddd06004240c30f7d9ffd
Mlockbug <energetichamster@gmail.com>
2022-11-28 11:50:30 +0000

    fixed camera lerp
    Stack leaks appeared again, then left without changing anything. I live in fear of them.

--- Assets/scripts/CharacterControl.cs
+++ Assets/scripts/CharacterControl.cs
@@ -14,6 +14,7 @@ public class CharacterControl : MonoBehaviour
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
+    public GameObject camWaypoint;
     public GameObject mazeCam;
     public GameObject mazeCamWaypoint;
     bool wantToRotate = true;
@@ -32,6 +33,7 @@ public class CharacterControl : MonoBehaviour
     bool speedCollected = false;
     bool parkourCollected = false;
     Vector3 respawnPoint = new Vector3(180f, 133f, 153f);
+    public GameObject endDoor;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -48,6 +50,9 @@ public class CharacterControl : MonoBehaviour
         {
             //transform.position += ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"))) * maxSpeed * Time.deltaTime;
 
+            //camWaypoint.transform.position = cam.transform.position;
+            //camWaypoint.transform.rotation = cam.transform.rotation;
+
             isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
             newVelocity = ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")));
 
@@ -57,6 +62,7 @@ public class CharacterControl : MonoBehaviour
                 transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
                 camRotation += Input.GetAxis("Mouse Y") * camRotationSpeed * -1;
                 cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+                camWaypoint.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
             }
 
             if (Input.GetKey(KeyCode.LeftShift) && sprintTimer>0f)
@@ -95,6 +101,7 @@ public class CharacterControl : MonoBehaviour
                 //mazeCam.transform.position = cam.transform.position;
                 cam.transform.position = Vector3.Lerp(cam.transform.position, mazeCamWaypoint.transform.position, Time.deltaTime);
                 cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, mazeCamWaypoint.transform.rotation, Time.deltaTime);
+                cam.transform.parent = null;
                 //cam.SetActive(false);
                 wantToRotate = false;
                 transform.rotation = Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f));
@@ -109,6 +116,9 @@ public class CharacterControl : MonoBehaviour
                 cam.SetActive(true);
                 mazeCam.SetActive(false);
                 wantToRotate = true;
+                cam.transform.parent = transform;
+                cam.transform.position = camWaypoint.transform.position;
+                cam.transform.localRotation = camWaypoint.transform.localRotation;
             }
 
             if  (speedCubes == 7)
@@ -124,11 +134,19 @@ public class CharacterControl : MonoBehaviour
                 transform.rotation = Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f));
                 wantToRotate = !wantToRotate;
             }*/
+
+            if (speedCollected && parkourCollected && mazeCollected)
+            {
+                endDoor.tag = "activated door";
+                endDoor.transform.position = Vector3.Lerp(endDoor.transform.position, new Vector3(205.71f, 135.38f, 141.38f), Time.deltaTime);
+            }
+            else
+            {
+                endDoor.transform.position = Vector3.Lerp(endDoor.transform.position, new Vector3(205.71f, 127.37f, 141.38f), Time.deltaTime);
+                endDoor.tag = "deactivated door";
+            }
+
         }
-
-        // will import these to make camera transition smooth if i have extra time
-
-
     }
 
 	private void FixedUpdate()
